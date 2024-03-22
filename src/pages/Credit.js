2a4e65fd4f122ed7afa0bda0a6d5dbfdd95cec87
yizhouchen <joe1542827496@gmail.com>
2024-03-22 09:48:15 -0700

    conWays-game

---
+++ src/pages/Credit.js
@@ -0,0 +1,16 @@
+function CreditPage() {
+  return (
+    <>
+      <h2>The entire React app is made by Mingyi Gao and Yizhou Chen</h2>
+      <span>Here is the Link to the GitHub repo: </span>
+      <a
+        href="https://github.com/leslie7717/CS5610-Mingyi/tree/main/hw/conways-game-by-mingyigao"
+        target="_blank"
+      >
+        The Link
+      </a>
+    </>
+  );
+}
+
+export default CreditPage;
---
+++ src/pages/Game.js
@@ -0,0 +1,14 @@
+import GameOfLife from "../components/GameOfLife";
+
+function GamePage() {
+  return (
+    <div>
+      <h1>The Game Page</h1>
+      <div>
+        <GameOfLife />
+      </div>
+    </div>
+  );
+}
+
+export default GamePage;
